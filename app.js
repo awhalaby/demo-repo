8f81d4f1fc9d1bd0968308f854ba4b33d7f73fc8
awhalaby <awhalaby@gmail.com>
2025-10-05 15:56:16 -0400

    following tutorial

---
+++ app.js
@@ -0,0 +1,13 @@
+function sayHello(name) {
+    console.log("Hello, " + name + "!");
+}
+
+//sayHello("Alex");
+
+var doof = "I am a global variable"; // global variable -- can be accessed from any file
+
+console.log(window.doof); // console.log is a global function -- can be accessed from any file
+
+//setTimeout() // another global function, used to delay execution of a function
+
+
